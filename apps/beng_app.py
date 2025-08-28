b7393a3a36f18563fc0ef1b468036a464698a1ee
Jedidiah Chibinga <jedidiah.gna@gmail.com>
2025-08-29 00:12:58 +0200

    add gpa calculation

--- apps/beng_app.py
+++ apps/beng_app.py
@@ -6,7 +6,7 @@ from apps import beng_functions
 data = pd.read_csv('data/gpa_data.csv')
 
 # List of possible grades
-grades_list = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D', 'P', 'F', 'S']
+grades_list = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'P', 'S']
 
 def app():
     # Set title
@@ -27,104 +27,194 @@ def app():
         # Display selected department as a header
         st.header(f"{selected_department} Engineering GPA")
 
+        # Add radio with specializations if EEE department selected
         if selected_department == 'Electrical & Electronics':
-            eletrical_specialty = st.radio(
+            electrical_specialty = st.radio(
                 "Select specialisation",
                 ["Electrical Machines and Power", "Electronics and Telecommunication"],
                 0,
                 horizontal=True)
 
-        # Choose if first year results should be added
-        include_first_year = st.toggle(
-            "Include 1st year results?",
-            True
-            )
+        gpa_col, grade_col = st.columns([1, 7])
 
-        if include_first_year:
-            years_list = beng_functions.years_of_study_list(data)
-        else:
-            years_list = beng_functions.years_of_study_list(data)
-            years_list = years_list[1:]
+        with grade_col:
+            # Choose if first year results should be added
+            include_first_year = st.toggle(
+                "Include 1st year results?",
+                True
+                )
+            
+            # Study years if first year toggle on
+            if include_first_year:
+                years_list = beng_functions.years_of_study_list(data)
 
-        for year in years_list:
-            with st.expander(f"{year} year"):
+            # Study years if first year toggle off
+            else:
+                years_list = beng_functions.years_of_study_list(data)
+                years_list = years_list[1:]
 
-                department_df = beng_functions.department_df(
-                    data, selected_department, year
+            # Year grades in list
+            overall_grades = {}
+
+            years_gpa_dict = {}
+
+            # Get courses for each year
+            for year in years_list:
+                # Create expander for each year
+                with st.expander(f"{year} year"):
+
+                    # Get df for specific year and department
+                    department_df = beng_functions.department_df(
+                        data, selected_department, year
+                        )
+
+                    # Electrical speciality set to None if selected department not EEE
+                    if selected_department != 'Electrical & Electronics':
+                        electrical_specialty = None
+
+                    # Get compulsory and elective courses dict
+                    compulsory_courses_dict, elective_courses_dict = beng_functions.courses_dict(
+                        selected_department, department_df, electrical_specialty)
+
+                    year_courses_grade_dict = {}
+                    iteration = 0
+                    if len(compulsory_courses_dict) > 0:
+                        # Heading for compulsory courses
+                        st.markdown(":red-background[Compulsory Courses]")
+
+                        # Create 2 columns to hold the data input 
+                        col1, col2 = st.columns([1,1])
+
+                        # Iterate through compulsory courses
+                        for (course_code, course_name) in compulsory_courses_dict.items():
+                            # Keep track of the iteration
+                            iteration = iteration + 1
+
+                            # Odd data iteration added to the left column
+                            if iteration % 2 != 0:
+                                with col1:
+                                    grade = st.radio(
+                                        label=f"{course_code} - {course_name}",
+                                        options=grades_list,
+                                        index=None,
+                                        horizontal=True,
+                                        key=course_code)
+
+                            # Even data iteration added to the right column
+                            else:
+                                with col2:
+                                    grade = st.radio(
+                                        label=f"{course_code} - {course_name}",
+                                        options=grades_list,
+                                        index=None,
+                                        horizontal=True,
+                                        key=course_code)
+
+                            # Course code and grade as dictionary
+                            course_grade_dict = {course_code:grade}
+
+                            # Add course code and grade to main dictionary
+                            year_courses_grade_dict.update(course_grade_dict)
+
+                    if len(elective_courses_dict) > 0:
+                        # Separate compulsory from elective courses
+                        st.divider()
+
+                        # Heading for elective courses
+                        st.markdown(":red-background[Elective Courses]")
+
+                        # Create 2 columns to hold the data input
+                        col1, col2 = st.columns([1,1])
+
+                        # Iterate through elective courses
+                        for (course_code, course_name) in elective_courses_dict.items():
+                            # Keep track of the iteration
+                            iteration = iteration + 1
+
+                            # Odd iteration data added to left column
+                            if iteration % 2 != 0:
+                                with col1:
+                                    grade = st.radio(
+                                        label=f"{course_code} - {course_name}",
+                                        options=grades_list,
+                                        index=None,
+                                        horizontal=True,
+                                        key=course_code)
+
+                            # Even iteration data added to the right column
+                            else:
+                                with col2:
+                                    grade = st.radio(
+                                        label=f"{course_code} - {course_name}",
+                                        options=grades_list,
+                                        index=None,
+                                        horizontal=True,
+                                        key=course_code)
+
+                            # Course code and grade as dictionary
+                            course_grade_dict = {course_code:grade}
+
+                            # Add course code and grade to main dictionary
+                            year_courses_grade_dict.update(course_grade_dict)
+
+                    courses_grade_dict = {year: year_courses_grade_dict}
+
+                overall_grades.update(courses_grade_dict)
+
+                course_credits_dict = beng_functions.course_credits_dict(
+                    selected_department, department_df, electrical_specialty)
+
+                year_gpa_dict = beng_functions.year_gpa(
+                    overall_grades, course_credits_dict, year)
+
+                years_gpa_dict.update(year_gpa_dict)
+
+
+        with gpa_col:
+            if sum(years_gpa_dict.values()) != 0:
+                gpas = years_gpa_dict.values()
+                overall_gpa = round(sum(gpas)/len([gpa for gpa in gpas if gpa > 0]), 3)
+
+                gpa_classification = beng_functions.gpa_classification(overall_gpa)
+
+                with st.container(border=True):
+                    st.metric(
+                        label="Overall GPA",
+                        value=overall_gpa,
                     )
+                    if gpa_classification == "Distinction":
+                        st.markdown(f":green-background[{gpa_classification}]")
+                    elif gpa_classification == "Merit":
+                        st.markdown(f":yellow-background[{gpa_classification}]")
+                    elif gpa_classification == "Credit":
+                        st.markdown(f":white-background[{gpa_classification}]")
+                    elif gpa_classification == "Pass":
+                        st.markdown(f":red-background[{gpa_classification}]")
 
-                if selected_department == 'Electrical & Electronics':
-                    eletrical_specialties_dict = {
-                        "Electrical Machines and Power": 'ET', 
-                        "Electronics and Telecommunication": "EMP"}
+            with st.container(border=True):
+                if include_first_year:
+                    st.metric(
+                        label="First Year GPA",
+                        value=years_gpa_dict['First'],
+                    )
+                gpa_classification = beng_functions.gpa_classification(overall_gpa)
 
-                    department_df = department_df[
-                        department_df['comment'] != eletrical_specialties_dict[eletrical_specialty]
-                        ]
+                st.metric(
+                    label="Second Year GPA",
+                    value=years_gpa_dict['Second'],
+                )
 
-                    compulsory_courses_dict = dict(zip(
-                        department_df[department_df['is_compulsory'] == 1]['course_code'],
-                        department_df[department_df['is_compulsory'] == 1]['course']
-                        ))
+                st.metric(
+                    label="Third Year GPA",
+                    value=years_gpa_dict['Third'],
+                )
 
-                    elective_courses_dict = dict(zip(
-                        department_df[department_df['is_compulsory'] == 0]['course_code'],
-                        department_df[department_df['is_compulsory'] == 0]['course']
-                        ))
+                st.metric(
+                    label="Fourth Year GPA",
+                    value=years_gpa_dict['Fourth'],
+                )
 
-                else:
-                    compulsory_courses_dict = dict(zip(
-                        department_df[department_df['is_compulsory'] == 1]['course_code'],
-                        department_df[department_df['is_compulsory'] == 1]['course']
-                        ))
-
-                    elective_courses_dict = dict(zip(
-                        department_df[department_df['is_compulsory'] == 0]['course_code'],
-                        department_df[department_df['is_compulsory'] == 0]['course']
-                        ))
-
-                iteration = 0
-                if len(compulsory_courses_dict) > 0:
-                    st.markdown(":red-background[Compulsory Courses]")
-                    col1, col2 = st.columns([1,1])
-                    for (course_code, course_name) in compulsory_courses_dict.items():
-                        iteration = iteration + 1
-                        if iteration % 2 != 0:
-                            with col1:
-                                grade = st.radio(
-                                    label=f"{course_code} - {course_name}",
-                                    options=grades_list,
-                                    index=None,
-                                    horizontal=True,
-                                    key=course_code)
-                        else:
-                            with col2:
-                                grade = st.radio(
-                                    label=f"{course_code} - {course_name}",
-                                    options=grades_list,
-                                    index=None,
-                                    horizontal=True,
-                                    key=course_code)
-
-                if len(elective_courses_dict) > 0:
-                    st.divider()
-                    st.markdown(":red-background[Elective Courses]")
-                    col1, col2 = st.columns([1,1])
-                    for (course_code, course_name) in elective_courses_dict.items():
-                        iteration = iteration + 1
-                        if iteration % 2 != 0:
-                            with col1:
-                                grade = st.radio(
-                                    label=f"{course_code} - {course_name}",
-                                    options=grades_list,
-                                    index=None,
-                                    horizontal=True,
-                                    key=course_code)
-                        else:
-                            with col2:
-                                grade = st.radio(
-                                    label=f"{course_code} - {course_name}",
-                                    options=grades_list,
-                                    index=None,
-                                    horizontal=True,
-                                    key=course_code)
+                st.metric(
+                    label="Fifth Year GPA",
+                    value=years_gpa_dict['Fifth'],
+                )
--- apps/beng_functions.py
+++ apps/beng_functions.py
@@ -49,5 +49,110 @@ def department_df(data, department, year):
     # Get specialised courses
     else:
         departments_df = data[(data['program'] == department)  & (data['year'] == year)]
-    
+
     return departments_df
+
+def courses_dict(selected_department, department_df, electrical_specialty):
+    # Get df for specific EEE specialization
+    if selected_department == 'Electrical & Electronics':
+        eletrical_specialties_dict = {
+            "Electrical Machines and Power": 'ET', 
+            "Electronics and Telecommunication": "EMP"}
+
+        # Get filter our df for specific EEE specialization
+        department_df = department_df[
+            department_df['comment'] != eletrical_specialties_dict[electrical_specialty]
+            ]
+
+        # Get dictionary for compulsory courses
+        compulsory_courses_dict = dict(zip(
+            department_df[department_df['is_compulsory'] == 1]['course_code'], # course_code as key
+            department_df[department_df['is_compulsory'] == 1]['course'] # course as value
+            ))
+
+        # Get dictionary for elective courses
+        elective_courses_dict = dict(zip(
+            department_df[department_df['is_compulsory'] == 0]['course_code'], # course_code as key
+            department_df[department_df['is_compulsory'] == 0]['course'] #course as value
+            ))
+
+    # Get other departments
+    else:
+        # Get dictionary for compulsory courses
+        compulsory_courses_dict = dict(zip(
+            department_df[department_df['is_compulsory'] == 1]['course_code'],
+            department_df[department_df['is_compulsory'] == 1]['course']
+            ))
+
+        # Get dictionary for elective courses
+        elective_courses_dict = dict(zip(
+            department_df[department_df['is_compulsory'] == 0]['course_code'],
+            department_df[department_df['is_compulsory'] == 0]['course']
+            ))
+
+    return compulsory_courses_dict, elective_courses_dict
+
+def course_credits_dict(selected_department, department_df, electrical_specialty):
+    # Get df for specific EEE specialization
+    if selected_department == 'Electrical & Electronics':
+        eletrical_specialties_dict = {
+            "Electrical Machines and Power": 'ET', 
+            "Electronics and Telecommunication": "EMP"}
+
+        # Get filter our df for specific EEE specialization
+        department_df = department_df[
+            department_df['comment'] != eletrical_specialties_dict[electrical_specialty]
+            ]
+
+        # Get dictionary for course credits
+        course_credits_dict = dict(zip(
+            department_df['course_code'], # course_code as key
+            department_df['course_credit'] # course_credit as value
+            ))
+
+    # Get other departments
+    else:
+        # Get dictionary for compulsory courses
+        course_credits_dict = dict(zip(
+            department_df['course_code'], # course_code as key
+            department_df['course_credit'] # course_credit as value
+            ))
+
+    return course_credits_dict
+
+def year_gpa(year_grades_dict, course_credits_dict, year):
+    # Grade and corresponding gpv
+    grade_gpv_dict = {
+        'A+': 5, 'A': 4, 'B+': 3.5, 'B': 3, 'C+': 2.37,
+        'C': 1, 'D+': 0, 'D': 0, 'P': 0, 'S': 0}
+
+    total_credits = []
+    cc_gpvs = []
+    for (course, grade) in year_grades_dict[year].items():
+        if grade is None:
+            pass
+        else:
+            grade_gpv = grade_gpv_dict[grade]
+            course_credit = course_credits_dict[course]
+            cc_gpv = grade_gpv * course_credit
+            cc_gpvs.append(cc_gpv)
+            total_credits.append(course_credit)
+
+    if sum(cc_gpvs) == 0 or sum(total_credits) == 0:
+        gpa = 0
+    else:
+        gpa = round(sum(cc_gpvs)/sum(total_credits),2)
+
+    year_gpa_dict = {year: gpa}
+
+    return year_gpa_dict
+
+def gpa_classification(overall_gpa):
+    if overall_gpa >= 3.75:
+        return "Distinction"
+    elif overall_gpa < 3.75 and overall_gpa >= 3.25:
+        return "Merit"
+    elif overall_gpa < 3.25 and overall_gpa >= 2.68:
+        return "Credit"
+    else:
+        return "Pass"
